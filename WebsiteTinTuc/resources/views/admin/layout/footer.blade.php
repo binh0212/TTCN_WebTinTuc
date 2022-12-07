3686f57c68ec80ac2fbbdabbe532aea5d8788e97
Your Name <you@example.com>
2022-12-07 16:26:15 +0700

    #2 fe 30%

--- WebsiteTinTuc/resources/views/admin/layout/footer.blade.php
+++ WebsiteTinTuc/resources/views/admin/layout/footer.blade.php
@@ -1,9 +1,6 @@
  <footer class="footer">
                     <div class="footer-content">
                      
-                        <span>
-                            <a href="" class="text-gray m-r-15">Term &amp; Conditions</a>
-                            <a href="" class="text-gray">Privacy &amp; Policy</a>
-                        </span>
+                        
                     </div>
 </footer>
